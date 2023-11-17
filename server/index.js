be59f4fa64d26ccf1349ac64999755f25466e453
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-11-17 14:18:52 +0530

    Initial Server Setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var cors = require("cors");
+const connectDB = require("./database");
+
+
+// connecting to database
+// connectDB();
+
+// setting up express server
+const app = express();
+let port = process.env.PORT;
+if (port == null || "") {
+  port = 9000;
+}
+
+app.use(cors());
+
+// We use this middle ware to read request body params
+app.use(express.json());
+
+// Avialable Routes
+// app.use('/api/auth', require('./routes/auth'));
+
+app.listen(port, () => {
+  console.log("Server started at port: " + port);
+});
